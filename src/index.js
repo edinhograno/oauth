7038a3a37fb352a7ec8107a0287b04c8be0db2a1
Edinho Grano <granomotorista@gmail.com>
2021-05-23 11:40:47 -0300

    commit inicial

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Oauth from './components/index';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Oauth />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
